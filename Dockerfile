4483ca4e2087fa8920e87d911bcdf2748b6f0ddf
yleko <116542488+yleko@users.noreply.github.com>
2022-10-24 00:14:58 -0700

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM golang:1.19-alpine AS xray
+RUN apk update && apk add --no-cache git
+WORKDIR /go/src/xray/core
+RUN git clone --progress https://github.com/XTLS/Xray-core.git . && \
+    go mod download && \
+    CGO_ENABLED=0 go build -o /tmp/xray -trimpath -ldflags "-s -w -buildid=" ./main
+
+FROM caddy:2.6.2-builder-alpine AS caddy
+RUN xcaddy build latest
+
+COPY --from=xray /tmp/xray /usr/bin 
+COPY entrypoint.sh /usr/bin
+COPY conf/ /conf/
+
+RUN set -ex \
+	&& mkdir -p /etc/caddy/ /etc/xray \
+	&& apk add --no-cache ca-certificates tor wget \
+	#&& chmod +x /usr/bin/xray \
+	#&& chmod +x /usr/bin/caddy \
+	&& chmod +x /usr/bin/entrypoint.sh 		
+
+CMD ["/usr/bin/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# args
+AUUID="754ca062-a341-4378-8b8c-ceb76d755c6c"
+#CADDYIndexPage="https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html"
+CADDYIndexPage="https://github.com/AYJCSGM/mikutap/archive/master.zip"
+ParameterSSENCYPT="chacha20-ietf-poly1305"
+#PORT=80
+
+# configs
+#mkdir -p /etc/caddy/ /etc/xray/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+mkdir -p /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+#wget $CADDYIndexPage -O /usr/share/caddy/index.html
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+cat /conf/Caddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+cat /conf/config.json | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/etc/xray/config.json
+
+# start
+tor &
+xray -config /etc/xray/config.json &
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
