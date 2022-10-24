25706780a7ae1a967dfd88dff3f5e2110f1d2597
yleko <116542488+yleko@users.noreply.github.com>
2022-10-24 01:10:31 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,10 @@ ParameterSSENCYPT="chacha20-ietf-poly1305"
 mkdir -p /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 #wget $CADDYIndexPage -O /usr/share/caddy/index.html
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-cat /etc/caddy/Caddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-cat /etc/xray/config.json | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/etc/xray/xray.json
+cat /etc/caddy/Caddy | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+cat /etc/xray/xray.json | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/etc/xray/config.json
 
 # start
 tor &
-xray -config /etc/xray/xray.json &
+xray -config /etc/xray/config.json &
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
